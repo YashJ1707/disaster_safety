673be1b4c13b6d1aa29e9c7e86aef518b3cb3ef1
YashJ1707 <yashjaybhaye1707@gmail.com>
2023-09-16 02:04:00 +0530

    addded changes

--- lib/screens/user/alert_page.dart
+++ lib/screens/user/alert_page.dart
@@ -1,4 +1,6 @@
-import 'package:disaster_safety/services/maps/maps_screen.dart';
+import 'package:disaster_safety/models/alert_model.dart';
+import 'package:disaster_safety/services/db.dart';
+import 'package:disaster_safety/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
@@ -11,14 +13,44 @@ class AlertPage extends StatelessWidget {
         title: Text("Alerts "),
         leading: Icon(Icons.warning),
       ),
-      // body: SingleChildScrollView(
-      //     child: SafeArea(
-      //   child: Container(
-      //     padding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
-      //     child: Column(children: []),
-      //   ),
-      // )),
-      body: MapsScreen(),
+      body: SingleChildScrollView(
+          child: SafeArea(
+        child: Container(
+            child: FutureBuilder(
+          future: DbMethods().getAlerts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    // var data = snapshot.data![index];
+                    Alert alert = snapshot.data![index];
+                    return GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: const Color(0xff764abc),
+                            child: Text(
+                              index.toString(),
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          title: Text(
+                              alert.title.substring(0, 1).toUpperCase() +
+                                  alert.title.substring(1)),
+                          subtitle: Text(alert.subtitle),
+                          trailing: const Icon(Icons.more_vert),
+                        ));
+                  },
+                );
+              }
+            }
+            return Loadings.staticLoader();
+          },
+        )),
+      )),
     );
   }
 }
